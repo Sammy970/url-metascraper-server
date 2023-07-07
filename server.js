e5b461e3dff656e03d53b006770b5168c29e5fdb
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-07-07 21:41:00 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+const metascraper = require("metascraper")([
+  require("metascraper-description")(),
+  //   require("metascraper-author")(),
+  //   require("metascraper-image")(),
+  //   require("metascraper-logo")(),
+  //   require("metascraper-title")(),
+  //   require("metascraper-url")(),
+]);
+
+const got = require("got");
+
+// Important Settings
+const app = express();
+const port = 3000 || process.env.PORT;
+
+app.get("/", async (req, res) => {
+  const link = req.query.link;
+  console.log(link);
+  const data = await fetchOGMetadata(link);
+  res.send(data);
+});
+
+async function fetchOGMetadata(url) {
+  try {
+    const { body: html, url: finalUrl } = await got(url);
+    const metadata = await metascraper({ html, url: finalUrl });
+
+    // console.log(metadata);
+
+    return metadata;
+  } catch (error) {
+    console.error("Error fetching Open Graph metadata:", error);
+    return null;
+  }
+}
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
